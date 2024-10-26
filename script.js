2eee069e60cbed717947b7382f6b8b1c7fd249e2
Leticia <110676739+mpleticia@users.noreply.github.com>
2024-10-25 23:07:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,69 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas = [
-    {
-        enunciado: "Você gosta da ideia de Inteligência Artificial?",
-        alternativas:["Sim","Não"]
+const perguntas = [     //serve para abrir lista de perguntas
+    {   //abre o objeto das perguntas
+        enunciado: "Você acha que IA vai dominar o mundo?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"A IA vai dominar o mundo"}, 
+
+            {texto: "Não",
+            afirmação:"A IA não dominará o mundo"}]
     },
-    {
-        enunciado: "Pergunta 2",
-        alternativas:["Alternativa 1","Alternativa 2"]
+    { 
+        enunciado: "A IA vai substituir os professores?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"A IA logo tomará o lugar dos professores"}, 
+                
+            {texto: "Não",
+            afirmação:"A IA jamais tomará o lugar dos professores"}]
     },
-    {
-        enunciado: "Pergunta 3",
-        alternativas:["Alternativa 1","Alternativa 2"]
-    }
+    { 
+        enunciado: "A IA está pronta para todos?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"Sim, a IA será útil e está pronta para todos"}, 
+                
+            {texto: "Não",
+            afirmação:"Não, a IA não é para o uso de todos"}]
+    },
 ]
 
 let posicao = 0;
 let perguntaAtual;
+let respostas = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmação;
+    respostas += afirmacoes + " ";
+    posicao++;
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Em 25 anos...";
+    textoResultado.textContent = respostas;
+    caixaAlternativa.textContent = "";
+}
+mostraPergunta();
